cf90a29ee9f4b24d8a7e0059ce129fdf8e48ad67
jalaj <jalaj@mail.com>
2021-08-12 18:33:21 +0530

    feat: :sparkles: Added sticky footer

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+// Form
+
+const form = document.querySelector('.header__form');
+const submitBtn = document.querySelector('.header__form__button');
+const formElements = form.elements;
+
+const getFormData = () => {
+	let data = {};
+  for (const element of formElements) {
+		console.log(element)
+    if (element.name.length > 0) {
+			if(!element.value)  return null;
+      data[element.name] = element.value;
+    }
+  }
+  return data;
+}
+
+submitBtn.onclick = (e) => {
+	e.preventDefault();
+	let data = getFormData();
+	if (data) {
+		localStorage.setItem('formdata', JSON.stringify(data));
+		alert('Form Data Saved!');
+	} else {
+		alert('Please fill all the fields!');
+	}
+};
+
+const populateForm = () => {
+	console.log("asasa")
+  if (localStorage.key('formdata')) {
+    const savedData = JSON.parse(localStorage.getItem('formdata'));
+    for (const element of formElements) {
+      if (element.name in savedData) {
+        element.value = savedData[element.name];
+      }
+    }
+  }
+};
+
+document.onload = populateForm();
+
+
 // Accordion
 
 const accordions = document.querySelectorAll(".curriculum__accordion");
@@ -5,7 +49,7 @@ const accordions = document.querySelectorAll(".curriculum__accordion");
 const openAccordion = (accordion) => {
 	const content = accordion.querySelector(".curriculum__accordion__content");
 	accordion.classList.add("accordion__active");
-	content.style.maxHeight = content.scrollHeight + "px";
+	content.style.maxHeight = (content.scrollHeight + 10) + "px";
 };
 
 const closeAccordion = (accordion) => {
@@ -43,3 +87,28 @@ indicators.forEach((item, i) => {
         currentTestimonial = i;
     })
 })
+
+// Sticky Footer
+
+// const form = document.querySelector('.header__form');
+
+let options= {
+	root: null,
+	rootMargin: '0px',
+	threshold: 0
+};
+
+const callbackFunction = (entries) => {
+	entries.forEach(entry=>{
+		let stickyFooter = document.querySelector('.sticky-footer');
+		
+		if(entry.isIntersecting === false) {
+			stickyFooter.classList.add('visible')
+		} else {
+			stickyFooter.classList.remove('visible')
+		}
+	})
+}
+
+let observer= new IntersectionObserver(callbackFunction,options);
+observer.observe(form);
